4d20f25dbb6d96af7b16986d5da6082064facd2f
Patrice Remy Lou D Virata <trish.virata@gmail.com>
2025-07-25 23:21:21 -0500

    logout fixed

--- feedbackapp/urls.py
+++ feedbackapp/urls.py
@@ -3,12 +3,13 @@ from django.urls import path
 from . import views
 from .views import CustomLoginView
 from .views import dashboard
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('feedback/', views.submit_feedback, name='submit_feedback'),
     path('thank-you/', views.thank_you, name='thank_you'),
     path('feedbacks/', views.feedback_list, name='feedback_list'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
 ]
--- feedbacksystem/settings.py
+++ feedbacksystem/settings.py
@@ -122,4 +122,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'feedback_list'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
