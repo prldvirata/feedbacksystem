c06c35e1f8084cf7cec68f0416669648293226dd
Patrice Remy Lou D Virata <trish.virata@gmail.com>
2025-08-13 08:16:27 -0500

    Revert "modals done!"
    This reverts commit 83de218ae4c2c1cdf40e3b7bc26e8e34a0c5a136.

--- feedbackapp/static/feedbackapp/ui_interactions.js
+++ feedbackapp/static/feedbackapp/ui_interactions.js
@@ -27,35 +27,7 @@ function setup() {
     })
     console.log("hello")
   })
-  document.getElementById('feedbackModal').addEventListener('show.bs.modal', function (event) {
-    const clickedElement = event.relatedTarget//specific cell
-    const row = feedback_table.row(clickedElement).node()//tr element
-    const feedbackData = feedback_table.row(row).data()//data object associated with tr element
-    if (!feedbackData){
-      console.error("could not retrieve data for clicked row.")
-    }
-    const feedbackModal = document.getElementById('feedbackModal')
-    populateModal(feedbackModal, feedbackData)
-  })
-
 }
 function applyFilters(){
-    
-}
-function init_modalListener(){}
-function populateModal(feedbackModal, feedbackData) {
-  const stars = score => '★'.repeat(score) + '☆'.repeat(5 - score);
-  feedbackModal.querySelector('.modal-title').textContent=`${feedbackData.name || 'Anonymous'}'s Review`
-  feedbackModal.querySelector('#overallRating').textContent=`Overall: ${stars(feedbackData.overall_rating)}`
-  feedbackModal.querySelector('#foodRating').textContent=`Food: ${stars(feedbackData.food_rating)}`
-  feedbackModal.querySelector('#serviceRating').textContent=`Service: ${stars(feedbackData.service_rating)}`
-  feedbackModal.querySelector('#ambienceRating').textContent=`Ambience: ${stars(feedbackData.ambience_rating)}`
-  feedbackModal.querySelector('#cleanlinessRating').textContent=`Cleanliness: ${stars(feedbackData.cleanliness_rating)}`
-  feedbackModal.querySelector('#feedbackComment').textContent=`${feedbackData.comment || 'Customer did not provide comment.'}`
-  feedbackModal.querySelector('#feedbackSuggestions').textContent=`${feedbackData.suggestions || 'Customer made no suggestions.'}`
-  if (feedbackData.recommendation == "Yes"){
-    feedbackModal.querySelector('#recommendation').innerHTML=`<strong class="text-success">Would Recommend</strong>`
-  } else {
-    feedbackModal.querySelector('#recommendation').innerHTML=`<strong class="text-danger">Would NOT Recommend</strong>`
-  }
+  
 }
